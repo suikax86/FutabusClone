09914145b899deb9e45e58d18f3df2c8e693d569
watermelonx86 <watermelonx86@outlook.com>
2024-03-04 21:04:50 +0700

    add dependency springdoc (Swagger-ui)

--- MDM-Project.Server/src/main/java/org/example/mdmprojectserver/Application.java
+++ MDM-Project.Server/src/main/java/org/example/mdmprojectserver/Application.java
@@ -1,9 +1,12 @@
 package org.example.mdmprojectserver;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "MDM Project API", version = "1.0", description = "MDM Project API"))
 public class Application {
 
     public static void main(String[] args) {
