4cdc42bf5571fd55ab6b67d81241f93ab51a684a
suika <suikax86@outlook.com>
2024-04-01 12:03:24 +0700

    add class WebConfig for CORs, call api register from font-end

--- MDM-Project.Client/src/containers/Auth/Register/Register.jsx
+++ MDM-Project.Client/src/containers/Auth/Register/Register.jsx
@@ -1,10 +1,47 @@
-import React, { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 import './Register.scss';
 
 import Header from '../../HomePages/Header';
 
 const Register = () => {
+    const navigate = useNavigate();
+    const [formData, setFormData] = useState({
+        phoneNumber: '',
+        email: '',
+        name: '',
+        password: ''
+    });
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setFormData({
+            ...formData,
+            [name]: value
+        });
+    };
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        try {
+            const response = await axios.post('http://localhost:8080/api/auth/register', formData);
+            if (response.status === 200) {
+                alert('User registered successfully');
+                navigate('/login');
+            } else {
+                const message = response.data.message || 'An error occurred while registering';
+
+                alert(message);
+            }
+        } catch (error) {
+            const message = error.response?.data?.message || 'An error occurred while registering';
+            alert(message)
+
+        }
+    };
+
     return (
         <div>
             <Header />
@@ -28,33 +65,48 @@ const Register = () => {
                             <div className='register-form'>
                                 <div className='register-form-title'>Đăng ký tài khoản</div>
                                 <div className='register-form-content'>
-                                    <form>
+                                    <form onSubmit={handleSubmit}>
                                         <div className='register-input-content'>
                                             <img src='https://futabus.vn/images/login/phone.svg'/>
-                                            <input type='text' 
-                                                className='form-control' 
-                                                placeholder='Nhập số điện thoại'
+                                            <input type='text'
+                                                   name='phoneNumber'
+                                                   className='form-control'
+                                                   placeholder='Nhập số điện thoại'
+                                                   value={formData.phoneNumber}
+                                                   onChange={handleChange}
                                             />
                                         </div>
                                         <div className='register-input-content'>
-                                            <img src='https://futabus.vn/images/icons/mail_send.svg' style={{width: '48px', height: '37.6px'}}/>
-                                            <input type='email' 
-                                                className='form-control' 
-                                                placeholder='Nhập Email'
+                                            <img src='https://futabus.vn/images/icons/mail_send.svg'
+                                                 style={{width: '48px', height: '37.6px'}}/>
+                                            <input type='email'
+                                                   name='email'
+                                                   className='form-control'
+                                                   placeholder='Nhập Email'
+                                                   value={formData.email}
+                                                   onChange={handleChange}
                                             />
                                         </div>
                                         <div className='register-input-content'>
-                                            <img src='https://www.shareicon.net/data/128x128/2016/05/24/770136_man_512x512.png' style={{width: '48px', height: '37.6px'}}/>
-                                            <input type='text' 
-                                                className='form-control' 
-                                                placeholder='Nhập họ tên'
+                                            <img
+                                                src='https://www.shareicon.net/data/128x128/2016/05/24/770136_man_512x512.png'
+                                                style={{width: '48px', height: '37.6px'}}/>
+                                            <input type='text'
+                                                   name='name'
+                                                   className='form-control'
+                                                   placeholder='Nhập họ tên'
+                                                   value={formData.name}
+                                                   onChange={handleChange}
                                             />
                                         </div>
                                         <div className='register-input-content'>
                                             <img src='https://futabus.vn/images/login/password.svg'/>
-                                            <input type='password' 
-                                                className='form-control' 
-                                                placeholder='Nhập mật khẩu'
+                                            <input type='password'
+                                                   name='password'
+                                                   className='form-control'
+                                                   placeholder='Nhập mật khẩu'
+                                                   value={formData.password}
+                                                   onChange={handleChange}
                                             />
                                         </div>
                                         <button className='btn-register'>
@@ -71,4 +123,4 @@ const Register = () => {
     )
 }
 
-export default Register
+export default Register;
